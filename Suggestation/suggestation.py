4056e0d07f8753c794bd7787217063f6cf77f4e8
Scott Foster <scottgarryfoster@googlemail.com>
2022-06-19 18:14:07 +0100

    Attempt 3 sending in another channel.

--- Suggestation/suggestation.py
+++ Suggestation/suggestation.py
@@ -1,5 +1,8 @@
 from discord import client
+from discord.utils import get
 from redbot.core import commands
+from redbot.core.utils import embed
+
 
 class Suggestation(commands.Cog):
     """My custom cog"""
@@ -11,7 +14,11 @@ class Suggestation(commands.Cog):
     async def suggest(self, ctx, suggestion):
         """This does stuff!"""
         # Your code will go here
-        await self.config.guild(557986764548079617).channel(800328370252415006)
+        #await self.config.guild(557986764548079617).channel(800328370252415006)
+
+        channel = get(ctx.guild.text_channels, 800328370252415006)
+        server = ctx.guild.id
+        msg = await channel.send(content=suggestion, embed=embed)
 
         #await client.get_guild(557986764548079617).get_channel(800328370252415006).send(suggestion)
-        await ctx.send("I can do stuff!" + suggestion)
+        #await ctx.send("I can do stuff!" + suggestion)
