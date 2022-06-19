ebcec0af4a333e1eeed9948b67ee2c20cec9c4dc
Scott Foster <scottgarryfoster@googlemail.com>
2022-06-19 17:59:55 +0100

    Suggestion now sends in another channel.

--- Suggestation/suggestation.py
+++ Suggestation/suggestation.py
@@ -1,3 +1,4 @@
+from discord import client
 from redbot.core import commands
 
 class Suggestation(commands.Cog):
@@ -10,7 +11,6 @@ class Suggestation(commands.Cog):
     async def suggest(self, ctx, suggestion):
         """This does stuff!"""
         # Your code will go here
-        self.all_commands.clear()
-        #await  ctx.send("", )
 
-        await ctx.send("I can do stuff!" + suggestion)
+        await client.get_guild(557986764548079617).get_channel(800328370252415006).send(suggestion)
+        #await ctx.send("I can do stuff!" + suggestion)
