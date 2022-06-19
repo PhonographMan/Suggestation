dc13eb81737e7d55bca993f6e86e22bc1fb88a41
Scott Foster <scottgarryfoster@googlemail.com>
2022-06-19 19:22:16 +0100

    Capitalised suggestion

--- Suggestation/suggestation.py
+++ Suggestation/suggestation.py
@@ -24,7 +24,7 @@ class Suggestation(commands.Cog):
 
         embed.add_field(name="Submitter", value=f"Walnut (she/her/they/them)#8008", inline=False)
 
-        embed.add_field(name="Suggestion", value=f"Special Heads for the months of May/June given out to recent graduates to celebrate our Dominionites RL Accomplishment", inline=False)
+        embed.add_field(name="SUGGESTION", value=f"Special Heads for the months of May/June given out to recent graduates to celebrate our Dominionites RL Accomplishment", inline=False)
         embed.add_field(name="IS YOUR IDEA AN EXISTING PLUGIN OR DATAPACK?", value=f"no", inline=False)
         embed.add_field(name="WHY SHOULD WE ADD THIS SUGGESTION?", value=f"I think it would be a fun way to celebrate members of our community as that's a major life event for many people. We have a lot of younger people on the server and it would be a nice way to show some support.", inline=False)
         embed.add_field(name="ANY OTHER USEFUL INFORMATION", value=f"Just an addition for community spirit", inline=False)
