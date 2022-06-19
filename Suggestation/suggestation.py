4d806a69bc66cc54b5c64deb4f5fe4a074afb152
Scott Foster <scottgarryfoster@googlemail.com>
2022-06-19 18:06:53 +0100

    Possible fix for different channels.

--- Suggestation/suggestation.py
+++ Suggestation/suggestation.py
@@ -11,6 +11,7 @@ class Suggestation(commands.Cog):
     async def suggest(self, ctx, suggestion):
         """This does stuff!"""
         # Your code will go here
+        await self.config.guild(557986764548079617).channel(800328370252415006)
 
-        await client.get_guild(557986764548079617).get_channel(800328370252415006).send(suggestion)
-        #await ctx.send("I can do stuff!" + suggestion)
+        #await client.get_guild(557986764548079617).get_channel(800328370252415006).send(suggestion)
+        await ctx.send("I can do stuff!" + suggestion)
