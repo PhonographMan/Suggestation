d2bb66030c1366caf6a210e2c30f162f43a67045
Scott Foster <scottgarryfoster@googlemail.com>
2022-06-20 21:50:16 +0100

    Added error message box and removed unnecessary channel checks.

--- Suggestation/suggestation.py
+++ Suggestation/suggestation.py
@@ -16,32 +16,24 @@ class Suggestation(commands.Cog):
 
     @commands.command()
     async def suggest(self, ctx: commands.Context, *, suggestion: str):
-        # 800328370252415006
-        #suggestionChannel = 732054706381127740
-        #suggestionPostChannel = 732054706381127740
         roleMention = "<@&988180927903592538>"
 
+        # Get a channel to send into
         sentChannel = ctx.channel
         configSentChannel = get(ctx.guild.text_channels, id=await self.config.sent_channel_id())
         if configSentChannel is not None:
             sentChannel = configSentChannel
 
+        # Get the channel to listen from
         listenChannel = ctx.channel
         configListenChannel = get(ctx.guild.text_channels, id=await self.config.listen_channel_id())
-        if not configListenChannel:
-            return await ctx.send("Uh oh, looks like your Admins haven't added the required channel.")
-
         if configListenChannel is not None:
-            await ctx.send(f"Listen channel is {configListenChannel} and ctx: {ctx.channel.id}")
-
             listenChannel = configListenChannel
-
 
         if ctx.channel != listenChannel:
             return
 
         embed = discord.Embed(
-            # color=await ctx.embed_colour(),
             color=discord.Color.from_rgb(255, 0, 255)
         )
 
@@ -61,21 +53,32 @@ class Suggestation(commands.Cog):
             currentContent = suggestion.split(f"**{fields[i]}**")
 
             if len(currentContent) > 2:
-                return await ctx.send(f"Entered too many of this field: **{fields[i]}**")
+                return await self.ErrorMessageBox(ctx,
+                                                  f"Entered too many of this field: **{fields[i]}**")
+                #return await ctx.send(f"Entered too many of this field: **{fields[i]}**")
 
             elif len(currentContent) == 1:
-                return await ctx.send(f"Field not found or nothing found within it, please enter something"
+                return await self.ErrorMessageBox(ctx,
+                                                  f"Field not found or nothing found within it, please enter something"
                                       f"even if it is N/A for the field: **{fields[i]}**")
+                #return await ctx.send(f"Field not found or nothing found within it, please enter something"
+                #                      f"even if it is N/A for the field: **{fields[i]}**")
 
             elif len(currentContent) > 2:
-                return await ctx.send(f"Field not found or nothing found within it, please enter something"
-                                      f"even if it is N/A for the field: **{fields[i]}**")
+                return await self.ErrorMessageBox(ctx,
+                                                  f"Field not found or nothing found within it, please enter something"
+                                                  f"even if it is N/A for the field: **{fields[i]}**")
+                #return await ctx.send(f"Field not found or nothing found within it, please enter something"
+                #                      f"even if it is N/A for the field: **{fields[i]}**")
 
             if i < len(fields) - 1:
                 currentContent = currentContent[1].split(f"**{fields[i + 1]}**")
 
                 if len(currentContent) == 1:
-                    return await ctx.send(f"Not enough fields in message. The field: {fields[i]} is not the last one.")
+                    return await self.ErrorMessageBox(ctx,
+                                                      f"Not enough fields in message. "
+                                                      f"The field: {fields[i]} is not the last one.")
+                    #return await ctx.send(f"Not enough fields in message. The field: {fields[i]} is not the last one.")
 
                 currentContent = currentContent[0]
 
@@ -90,11 +93,24 @@ class Suggestation(commands.Cog):
                         value=roleMention,
                         inline=False)
 
-        #channel = get(ctx.guild.text_channels, id=sentChannel)
         msg = await sentChannel.send("", embed=embed)
         await msg.add_reaction("<:emoji:731293934822883429>")
         await msg.add_reaction("<:emoji:731293934856175687>")
         await ctx.message.delete()
+
+    async def ErrorMessageBox(
+            self,
+            ctx: commands.Context,
+            suggestion: str
+    ):
+        embed = discord.Embed(
+            author="Something went wrong",
+            color=discord.Color.from_rgb(255, 0, 0),
+            description=suggestion
+        )
+
+        return await ctx.send("", embed=embed)
+
 
     @commands.command(name="suggestation")
     async def setsuggest_setglobal_suggestation_staffcommands(
