c52890d843b7f1e5c951432587032ea6482653b7
Scott Foster <scottgarryfoster@googlemail.com>
2022-06-21 21:16:58 +0100

    Reloaded Settings.

--- Suggestation/suggestation.py
+++ Suggestation/suggestation.py
@@ -15,7 +15,7 @@ class Suggestation(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=5654645645645645, force_registration=True)
+        self.config = Config.get_conf(self, identifier=56546456456456553, force_registration=True)
         self.config.register_guild(
             listen_channel_id=None,
             sent_channel_id=None,
